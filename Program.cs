cc03d4d6b97cab9e2aadbc7b3240651d5567bfdb
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-05 20:08:12 +0700

    lab9 project

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ban hay nhap vao 1 so");
+        int so = Convert.ToInt32(Console.ReadLine());
+
+        if (0 < so && so < 5)
+        {
+            switch (so) // Changed 'Switch' to 'switch'
+            {
+                case 1:
+                    Console.WriteLine("*");
+                    Console.WriteLine("* *");
+                    Console.WriteLine("* * *");
+                    Console.WriteLine("* * * *");
+                    Console.WriteLine("* * * * *");
+                    Console.WriteLine("* * * * * *");
+                    break;
+                case 2:
+                    Console.WriteLine("* * * * * * * * * *");
+                    Console.WriteLine("* * * * * * * * * *");
+                    Console.WriteLine("* * * * * * * * * *");
+                    Console.WriteLine("* * * * * * * * * *");
+                    break;
+                case 3:
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * *");
+                    Console.WriteLine("* * * *");
+                    Console.WriteLine("* * *");
+                    Console.WriteLine("* *");
+                    Console.WriteLine("*");
+                    break;
+                default:
+                    Console.WriteLine("Gia tri khong nam trong pham vi xu ly.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Gia tri nhap khong hop le. Vui long nhap so tu 1 den 4.");
+        }
+    }
+}
